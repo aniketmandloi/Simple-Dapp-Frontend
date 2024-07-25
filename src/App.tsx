af4c844a6d8d3872eabb5056ec166f6b9f78e1ee
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-07-25 16:07:50 +0530

    added columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+        </div>
+      </div>
     </div>
   );
 }
